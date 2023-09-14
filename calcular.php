8c9efd4f01e55a7fc29870bf69cc5d550df5a853
Andres Felipe Sanchez Manjarres <piposanchezman@gmail.com>
2023-09-13 22:17:54 -0500

    Co-authored-by: Sergio Serrano <SergioSerranoDv@users.noreply.github.com>

--- calcular.php
+++ calcular.php
@@ -1,40 +1,66 @@
 <?php
-$file = fopen("bricks.csv", "r");
-$data = array();
+function readCSV($filename)
+{
+    $file = fopen($filename, "r");
+    $data = array();
 
-while (($row = fgetcsv($file)) !== FALSE) {
-    $data[] = $row;
+    while (($row = fgetcsv($file)) !== FALSE) {
+        $data[] = $row;
+    }
+
+    fclose($file);
+
+    return $data;
 }
 
-fclose($file);
+function BrickRequirements($data, $bricksT, $wallT, $horizontalG, $verticalG)
+{
+    $cl = 0;
+
+    foreach ($data as $element) {
+        if ($element[0] === $bricksT) {
+            $width = ($wallT === "soga") ? $element[1] : $element[2];
+            $height = $element[3];
+            $cl += 1 / (($width + $horizontalG) * ($height + $verticalG));
+        }
+    }
+
+    return $cl;
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $ancho = $_POST["ancho"];
-    $alto = $_POST["alto"];
-    $tladrillo = $_POST["tladrillo"];
-    $tmuro = $_POST["tmuro"];
-    $jhorizontal = $_POST["jhorizontal"];
-    $jvertical = $_POST["jvertical"];
+    $widthW = $_POST["widthW"];
+    $heightW = $_POST["heightW"];
+    $bricksT = $_POST["bricksT"];
+    $wallT = $_POST["wallT"];
+    $horizontalG = $_POST["horizontalG"];
+    $verticalG = $_POST["verticalG"];
+    $costBrick = $_POST["costBrick"];
 
-    echo "Ancho: $ancho<br>";
-    echo "Alto: $alto<br>";
-    echo "Tipo de Ladrillo: $tladrillo<br>";
-    echo "Tipo de muro: $tmuro<br>";
-    echo "Junta Horizontal: $jhorizontal<br>";
-    echo "Junta Vertical: $jvertical<br>";
+    $data = readCSV("bricks.csv");
+    $cl = BrickRequirements($data, $bricksT, $wallT, $horizontalG, $verticalG);
 
-    if ($tmuro === "soga") {
-        $cl = 0;
-        foreach ($data as $element) {
-            if ($element[0] === $tladrillo) {
-                $width = $element[1];
-                $espesor = $element[2];
-                $height = $element[3];
-                $cl += 1 / (($width + $jhorizontal) * ($height + $jvertical));
-            }
-        }
-        $rounded = round($cl);
-        echo "La cantidad de ladrillos = $rounded<br>";
+    if ($cl > 0) {
+        // Redondear la cantidad de ladrillos por m^2
+        $rounded = ceil($cl);
+        echo "Cantidad de ladrillos por m^2 = $rounded<br>";
+        // Calcular el desperdicio del 5%
+        $waste = $rounded * 0.05;
+        echo "Desperdicio de ladrillos (5%) = $waste<br>";
+        // Calcular la cantidad de ladrillos por m^2 (considerando 5% de desperdicio)
+        $quantityBricks = ceil($rounded + $waste);
+        echo "Cantidad de ladrillos + desperdicio = $quantityBricks<br>";
+        // Calcular el área del muro
+        $wallArea = $widthW * $heightW;
+        echo "Area total del muro = $wallArea<br>";
+        // Calcular la cantidad total de ladrillos necesarios para el muro
+        $totalBricks = $quantityBricks * $wallArea;
+        echo "Cantidad total de ladrillos = $totalBricks<br>";
+        // Calcular el costo total de los ladrillos
+        $totalCost = $totalBricks * $costBrick;
+        echo "Costo total de los ladrillos = $totalCost<br>";
+    } else {
+        echo "No se encontraron ladrillos del tipo seleccionado";
     }
 }
 ?>
--- index.php
+++ index.php
@@ -13,36 +13,47 @@
         </div>
         <div class="mx-auto max-w-2xl text-center">
             <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Constructora SAYA</h2>
-            <p class="mt-2 text-lg leading-8 text-gray-600">Estamos comprometidos contigo, tu bienestar económico es nuestra preocupación</p>
+            <p class="mt-2 text-lg leading-8 text-gray-600">Innovación en construcción, calidad en cada obra.</p>
         </div>  
         <form method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
-            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                <div> 
-                    <label class="block text-sm font-semibold leading-6 text-gray-900">Longitud del muro:</label>
-                    <div class="mt-2.5">
-                        <input type="phone" name="ancho" placeholder="Ancho (m)" class="block w-1/2 rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                        <input type="phone" name="alto" placeholder="Alto (m)" class="block w-1/2 rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <div class="relative flex flex-col gap-6">
+                <div class="grid grid-cols-2 items-center"> 
+                    <label class="text-sm font-semibold leading-6 text-gray-900">Longitud del muro:</label>
+                    <div class="inline-flex gap-2">
+                        <input type="phone" name="widthW" placeholder="Ancho (m)" class="block w-1/2 rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        <input type="phone" name="heightW" placeholder="Alto (m)" class="block w-1/2 rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                     </div>
                 </div>    
-                <div>
-                    <label class="block text-sm font-semibold leading-6 text-gray-900">Tipo de ladrillo</label>
-                    <div class="mt-2.5">
-                    <select name="tladrillo" class="block w-full rounded-md border-0 bg-transparent bg-none px-3.5 py-2 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                <div class="grid grid-cols-2 items-center">
+                    <label class="text-sm font-semibold leading-6 text-gray-900">Tipo de ladrillo:</label>
+                    <select name="bricksT" class="block w-full rounded-md border-0 bg-transparent bg-none px-3 py-2 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
                         <option value="kingkong">King Kong</option>
                         <option value="modular">Modular</option>
                     </select>
-                </div>     
-                        <input type="radio" name="tmuro" value="soga">Soga</option>
-                        <input type="radio" name="tmuro" value="tizon">Tizon</option>
-                    <label>Espesor Juntas</label>
-                        <input type="phone" step="any" name="jhorizontal" placeholder="Horizontal (m)">
-                        <input type="phone" step="any" name="jvertical" placeholder="Vertical (m)">
-                    <label>Costo ladrillo</label>
-                    <input type="price" name="costo">
-                    <button class="px-2 rounded-full border-2 border-orange-950">Calcular</button>
-                    <?php include 'calcular.php'; ?>
-            </div>
+                </div>
+                <div class="grid grid-cols-2 items-center"> 
+                    <label class="text-sm font-semibold leading-6 text-gray-900">Tipo de muro:</label>
+                    <div class="inline-flex gap-4">    
+                        <input type="radio" name="wallT" value="soga">Soga</option>
+                        <input type="radio" name="wallT" value="tizon">Tizon</option>
+                    </div>
+                </div>
+                <div class="grid grid-cols-2 items-center">     
+                    <label class="text-sm font-semibold leading-6 text-gray-900">Espesor juntas:</label>
+                    <div class="inline-flex gap-2">
+                        <input type="phone" step="any" name="horizontalG" placeholder="Horizontal (m)" class="block w-1/2 rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        <input type="phone" step="any" name="verticalG" placeholder="Vertical (m)" class="block w-1/2 rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+                <div class="grid grid-cols-2 items-center">    
+                    <label class="text-sm font-semibold leading-6 text-gray-900">Costo ladrillo</label>
+                    <input type="price" name="costBrick" placeholder="10000" class="block w-full rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+                <div class="grid grid-cols-1 place-items-center mt-8">      
+                    <button class="w-1/4 px-2 rounded-full border-2 border-orange-950">Calcular</button>
+                </div>
+                <?php include 'calcular.php'; ?>
         </form>
-    </div> 
+    </div>   
 </body>
 </html>
